7be9e9c60d2c0cefc7cdff74bc28c835064d6d71
Arthur Pedroti <arthurpedroti@gmail.com>
2021-08-31 11:52:07 -0300

    comments component

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import ReactDisqusComments from 'react-disqus-comments'
+
 import * as S from './styles'
 
-const Comments = () => (
-  <S.Wrapper>
-    <h1>Comments</h1>
-  </S.Wrapper>
-)
+const Comments = ({ url, title }) => {
+  const completeURL = `https://dev.arthurpedroti.com.br${url}`
 
+  return (
+    <S.Wrapper>
+      <S.CommentsTitle>Comentários</S.CommentsTitle>
+      <ReactDisqusComments
+        shortname="arthurpedroti"
+        identifier={completeURL}
+        title={title}
+        url={completeURL}
+      />
+    </S.Wrapper>
+  )
+}
+
+Comments.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
+}
 export default Comments
