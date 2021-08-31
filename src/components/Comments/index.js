714b4f762ba07b77265a59a49bb29b6e3f82fa8d
Arthur Pedroti <arthurpedroti@gmail.com>
2021-08-31 11:00:54 -0300

    recommended posts component

---
+++ src/components/Comments/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as S from './styles'
+
+const Comments = () => (
+  <S.Wrapper>
+    <h1>Comments</h1>
+  </S.Wrapper>
+)
+
+export default Comments
--- src/components/RecommendedPosts/index.js
+++ src/components/RecommendedPosts/index.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import propTypes from 'prop-types'
 import * as S from './styles'
 
-const RecommendedPosts = () => (
+const RecommendedPosts = ({ next, previous }) => (
   <S.Wrapper>
-    <h1>RecommendedPosts</h1>
+    {previous && (
+      <S.RecommendedLink to={previous.fields.slug} className="previous">
+        {previous.frontmatter.title}
+      </S.RecommendedLink>
+    )}
+    {next && (
+      <S.RecommendedLink to={next.fields.slug} className="next">
+        {next.frontmatter.title}
+      </S.RecommendedLink>
+    )}
   </S.Wrapper>
 )
+
+RecommendedPosts.propTypes = {
+  next: propTypes.shape({
+    frontmatter: propTypes.shape({
+      title: propTypes.string.isRequired
+    }),
+    fields: propTypes.shape({
+      slug: propTypes.string.isRequired
+    })
+  }),
+  previous: propTypes.shape({
+    frontmatter: propTypes.shape({
+      title: propTypes.string.isRequired
+    }),
+    fields: propTypes.shape({
+      slug: propTypes.string.isRequired
+    })
+  })
+}
 
 export default RecommendedPosts
