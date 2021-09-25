aa2243a19c47212b94ee25bada83b5c532e138f0
Arthur Pedroti <arthurpedroti@gmail.com>
2021-09-25 20:35:25 -0300

    gatsby image

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,23 +1,18 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+
 import * as S from './styles'
 
 const Avatar = () => {
-  const { avatarImage } = useStaticQuery(
-    graphql`
-      query {
-        avatarImage: file(relativePath: { eq: "profile-photo.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 60) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `
+  return (
+    <S.AvatarWrapper>
+      <StaticImage
+        src="../../images/profile-photo.png"
+        alt="Arthur Pedroti"
+        placeholder="blurred"
+      />
+    </S.AvatarWrapper>
   )
-
-  return <S.AvatarWrapper fixed={avatarImage.childImageSharp.fluid} />
 }
 
 export default Avatar
